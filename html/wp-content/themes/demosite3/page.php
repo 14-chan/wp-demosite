e992bc3e787577b250e33896f313b9ca47fa0375
14chan <kubospa2003@gmail.com>
2024-10-02 12:18:51 +0900

    senventh commit

---
+++ html/wp-content/themes/demosite3/page.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<div class="l-container">
+    <h2 class="c-contact__ttl">CONTACT</h2>
+    <?php echo apply_shortcodes('[contact-form-7 id="f032d65" title="無題"]'); ?>
+</div>
+
+<?php get_footer(); ?>
